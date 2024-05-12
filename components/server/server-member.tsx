59d353b5a56592bb2d5b69d7998c12f67fefbfef
Orange <orangegamerz2124@gmail.com>
2024-05-12 16:08:17 +0530

    change

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -18,7 +18,7 @@ export const ServerMember=({member,server}:ServerMemberProps)=>{
     const router=useRouter();
     const icon=roleIconMap[member.role];
     const onClick=()=>{
-        router.push(`/servers/${params.serverId}/conversations/${member.id}`)
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
     }
     return(
     <button onClick={onClick}
